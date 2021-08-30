e06ff53b2249be84c14f4f9986fa72897ae7befe
Tobias Kadelka <t.kadelka@fz-juelich.de>
2021-08-30 14:09:56 +0200

    fix timecounter-bug, make variable names humanreadable, add further TODOs

--- tipuesearch_lite.js
+++ tipuesearch_lite.js
@@ -7,7 +7,7 @@ Tipue Search Lite is released under the MIT License
 //Stop words list from http://www.ranks.nl/stopwords
 var tipuesearch_stop_words = ["a", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"];
 
-// Weighting
+// Weighting for tipue KMP algorithm
 var tipuesearch_weight = {'weight': [
      {'url': 'http://www.tipue.com', 'score': 60},
      {'url': 'http://www.tipue.com/search', 'score': 60},
@@ -15,7 +15,7 @@ var tipuesearch_weight = {'weight': [
      {'url': 'http://www.tipue.com/support', 'score': 20}
 ]};
 
-// Illogical stemming
+// these words get replaced by stem if they are searched (e-mail shows results for email)
 var tipuesearch_stem = {'words': [
      {'word': 'e-mail', 'stem': 'email'},
      {'word': 'javascript', 'stem': 'jquery'},
@@ -38,18 +38,25 @@ window.onload = function execute(){
         "showURL": false,
         "wholeWords": true
     };
-    var tipuesearch_t_c = 0;
+
+	// TODO: possibly better change var above, instead of flag if it is still relevant
+    // remember if title gets set (to modify tab-name just once)
+    var tabTitleFlag = true
+
     var tipue_search_w = "";
     if (set.newWindow) {
+    	// TODO: this gets never modified, just used two times in a string.
         tipue_search_w = " target='_blank'";
     }
     let params = new URLSearchParams(document.location.search.substring(1));
 
+    // read search box, call search
     if (params.get("q")) {
         document.getElementById("tipue_search_input").value = params.get("q");
         getTipueSearch();
     }
 
+	// add to history
     document.getElementById('tipue_search_input').form.onsubmit = function() {
         getTipueSearch();
 
@@ -71,101 +78,123 @@ window.onload = function execute(){
     }
 
     function getTipueSearch() {
-        var startTimer = new Date().getTime();
+
+		// Timer for showTime
+		var startTimer = new Date().getTime();
+		// string for html output
+
         var out = "";
-        var show_stop = false;
+        // if only stop words (common words) get found, prints out "nothing found. Common words get ignored"
+        var stopWordsFoundFlag = false;
+        // TODO: understand/delete standard: (d.match("^\"") && d.match("\"$")) || (d.match("^'") && d.match("'$")))
         var standard = true;
-        var c = 0;
+		// counts amount of results/pages found
+        var resultCounter = 0;
+        // found saves objects about pages that are found
         var found = [];
-        var d_o = document.getElementById("tipue_search_input").value;
-        d_o = d_o.replace(/\+/g, " ").replace(/\s\s+/g, " ");
-        d_o = d_o.trim();
-        var d = d_o.toLowerCase();
-        if ((d.match("^\"") && d.match("\"$")) ||
-        (d.match("^'") && d.match("'$"))) {
+
+        // search word
+        var searchBoxInput = document.getElementById("tipue_search_input").value;
+        searchBoxInput = searchBoxInput.replace(/\+/g, " ").replace(/\s\s+/g, " ");
+        searchBoxInput = searchBoxInput.trim();
+        var temp_searchWord = searchBoxInput.toLowerCase();
+
+		// if special break characters get used
+        if ((temp_searchWord.match("^\"") && temp_searchWord.match("\"$")) ||
+        (temp_searchWord.match("^'") && temp_searchWord.match("'$"))) {
             standard = false;
         }
-        var d_w = d.split(" ");
+        var searchWordList = temp_searchWord.split(" ");
 
+		// MODIFY SEARCH WORDS - BLOCK
+		// this whole if(standard) deletes the stop words from the search
         if (standard) {
-            d = "";
-            for (var i = 0; i < d_w.length; i++) {
+        	// TODO: start with searchWordList, iterate over stop-word and delete them from temporary list
+        	// saves temporarly the search words from d_w (if they are not common words)
+            temp_searchWord = "";
+            // for each word check if it is a stop word (common word)
+            for (var i = 0; i < searchWordList.length; i++) {
+            	// this is true as long as no common word gets found.
                 var a_w = true;
                 for (var f = 0; f < tipuesearch_stop_words.length; f++) {
-                    if (d_w[i] == tipuesearch_stop_words[f]) {
+                    if (searchWordList[i] == tipuesearch_stop_words[f]) {
                         a_w = false;
-                        show_stop = true;
+                        // saves if a stop word was found at some point
+                        stopWordsFoundFlag = true;
                     }
                 }
+                // if no stop word, add it to list of search words
                 if (a_w) {
-                    d = d + " " + d_w[i];
+                  temp_searchWord += " " + searchWordList[i];
                 }
             }
-            d = d.trim();
-            d_w = d.split(" ");
+            temp_searchWord = temp_searchWord.trim();
+            searchWordList = temp_searchWord.split(" ");
+		// TODO: not sure about the else case, seems to depend on d.match(...)
         } else {
-            d = d.substring(1, d.length - 1);
+            temp_searchWord = temp_searchWord.substring(1, temp_searchWord.length - 1);
         }
-        if (d.length >= set.minimumLength) {
+
+		// SEARCH TROUGH PAGES - BLOCK
+        // minimal amount of characters searched for
+        if (temp_searchWord.length >= set.minimumLength) {
             if (standard) {
-                var d_t = d;
-                for (var i = 0; i < d_w.length; i++) {
+                // TODO: d_t is temp var?
+                var d_t = temp_searchWord;
+                // TODO: EITHER DELETE THIS AND THE REPLACEMENT WORDS, OR MOVE UP TO OTHER BLOCK
+                // this loop seems to replace words like e-mail with email. I am not sure, if this big if (standard) is needed, because if nothing to replace, the loop gets ignored anyways?
+                for (var i = 0; i < searchWordList.length; i++) {
                     for (var f = 0; f < tipuesearch_stem.words.length; f++) {
-                        if (d_w[i] == tipuesearch_stem.words[f].word) {
+                        if (searchWordList[i] == tipuesearch_stem.words[f].word) {
                             d_t = d_t + " " + tipuesearch_stem.words[f].stem;
                         }
                     }
                 }
-                d_w = d_t.split(" ");
-                for (var i = 0; i < tipuesearch.pages.length; i++) {
-                    var score = 0;
-                    var s_t = tipuesearch.pages[i].text;
+            }
+            searchWordList = d_t.split(" ");
+            // TOODO: check if right:
+            // seems to loop over every single page of the wiki (?), gets the text, calculates score and pushes result to "found"
+            for (var i = 0; i < tipuesearch.pages.length; i++) {
+                var score = 0;
+                // text of current wikitext
+                var s_t = tipuesearch.pages[i].text;
 
-                    score = tipue_KMP(d_w, s_t, set, i);
+	            // this searches for all the search words in the text of the current wikipage
+                score = tipue_KMP(searchWordList, s_t, set, i);
 
-                    if (score != 0) {
-                        found.push({
-                            "score": score,
-                            "title": tipuesearch.pages[i].title,
-                            "desc": s_t,
-                            "url": tipuesearch.pages[i].url,
-                            "note": tipuesearch.pages[i].note
-                        });
-                        c++;
-                    }
-                }
-            } else {
-                d_w = d.split(" ");
-                for (var i = 0; i < tipuesearch.pages.length; i++) {
-                    var score = 0;
-                    var s_t = tipuesearch.pages[i].text;
-
-                    score = tipue_KMP(d_w, s_t, set, i);
-
-                    if (score != 0) {
-                        found.push({
-                            "score": score,
-                            "title": tipuesearch.pages[i].title,
-                            "desc": s_t,
-                            "url": tipuesearch.pages[i].url,
-                            "note": tipuesearch.pages[i].note
-                        });
-                        c++;
-                    }
+				// if the page contains search words, save the title etc
+                if (score != 0) {
+                    found.push({
+                        "score": score,
+                        "title": tipuesearch.pages[i].title,
+                        "desc": s_t,
+                        "url": tipuesearch.pages[i].url,
+                        "note": tipuesearch.pages[i].note
+                    });
+                    // counts findings
+                    resultCounter++;
                 }
             }
-            if (c != 0) {
-                if (set.showTitleCount && tipuesearch_t_c == 0) {
+
+            // building up the web-page that shows the search results
+            if (resultCounter != 0) {
+            	// add the number of results found to document title (add number to tab-name)
+                if (set.showTitleCount && tabTitleFlag) {
                     var title = document.title;
-                    document.title = "(" + c + ") " + title;
-                    tipuesearch_t_c++;
+                    document.title = "(" + resultCounter + ") " + title;
+                    tabTitleFlag = false;
                 }
-                if (c == 1) {
+
+				// "X results found" output line
+                if (resultCounter == 1) {
                     out += "<div id='tipue_search_results_count'>1 result";
                 } else {
-                    var c_c = c.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    var c_c = resultCounter.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    //TODO: why replace this weird expression, should be just an integer
                     out += "<div id='tipue_search_results_count'>" + c_c + " results";
                 }
+
+                // show how long search took
                 if (set.showTime) {
                     var endTimer = new Date().getTime();
                     var time = (endTimer - startTimer) / 1000;
@@ -173,10 +202,11 @@ window.onload = function execute(){
                 }
                 out += "</div>";
 
+				// sorts "found"-array by score
                 found.sort(function(a, b) {
                     return b.score - a.score
                 });
-                var l_o = 0;
+
                 for (var i = 0; i < found.length; i++) {
                     out += "<div class='tipue_search_result'>";
                     out += "<div class='tipue_search_content_title'><a href='" + found[i].url + "'" + tipue_search_w + ">" + found[i].title + "</a></div>";
@@ -188,9 +218,10 @@ window.onload = function execute(){
                     }
                     if (found[i].desc) {
                         var t = found[i].desc;
+                        // TODO: searchWordList does not get modified, so it does not need to be set again
+                        searchWordList = temp_searchWord.split(" ");
                         if (set.showContext) {
-                            d_w = d.split(" ");
-                            var s_1 = found[i].desc.toLowerCase().indexOf(d_w[0]);
+                            var s_1 = found[i].desc.toLowerCase().indexOf(searchWordList[0]);
                             if (s_1 > set.contextStart) {
                                 var t_1 = t.substr(s_1 - set.contextBuffer);
                                 var s_2 = t_1.indexOf(" ");
@@ -202,15 +233,14 @@ window.onload = function execute(){
                             }
                         }
                         if (standard) {
-                            d_w = d.split(" ");
-                            for (var f = 0; f < d_w.length; f++) {
+                            for (var f = 0; f < searchWordList.length; f++) {
                                 if (set.highlightTerms) {
-                                    var patr = new RegExp("(" + d_w[f] + ")", "gi");
+                                    var patr = new RegExp("(" + searchWordList[f] + ")", "gi");
                                     t = t.replace(patr, "<h0011>$1<h0012>");
                                 }
                             }
                         } else if (set.highlightTerms) {
-                            var patr = new RegExp("(" + d + ")", "gi");
+                            var patr = new RegExp("(" + temp_searchWord + ")", "gi");
                             t = t.replace(patr, "<span class=\"tipue_search_content_bold\">$1</span>");
                         }
                         var t_d = "";
@@ -234,20 +264,15 @@ window.onload = function execute(){
                         out += "<div class='tipue_search_note'>" + found[i].note + "</div>";
                     }
                     out += "</div>";
-                    l_o++;
                 }
-            } else {
+            } else { // if words don't give results
                 out += "<div id='tipue_search_error'>Nothing found.</div>";
             }
         } else {
-            if (show_stop) {
+            if (stopWordsFoundFlag) { // for example if search is just "yourself"
                 out += "<div id='tipue_search_error'>Nothing found. Common words are largely ignored.</div>";
             } else {
-                if (set.minimumLength == 1) {
-                    out += "<div id='tipue_search_error'>Search should be one character or more.</div>";
-                } else {
-                    out += "<div id='tipue_search_error'>Search should be " + set.minimumLength + " characters or more.</div>";
-                }
+                out += "<div id='tipue_search_error'>Search should be " + set.minimumLength + " or more characters.</div>";
             }
         }
         document.getElementById("tipue_search_content").innerHTML = out;
@@ -302,39 +327,41 @@ window.onload = function execute(){
         return cnt;
     }
 
-    function tipue_KMP(d_w, s_text, set, i){
+    function tipue_KMP(searchWordList, s_text, set, i){
         var score = 0;
-        for(var f=0;f<d_w.length;f++){
-            var pre_tab = KMP_prefix(d_w[f], d_w[f].length);
-            var match_cnt = KMP_search(d_w[f], pre_tab, tipuesearch.pages[i].title);
+        for(var f = 0; f < searchWordList.length; f++){
+            var pre_tab = KMP_prefix(searchWordList[f], searchWordList[f].length);
+            var match_cnt = KMP_search(searchWordList[f], pre_tab, tipuesearch.pages[i].title);
             if(match_cnt != 0){
-                score+=(20*match_cnt);
+                score += (20 * match_cnt);
             }
-            match_cnt = KMP_search(d_w[f], pre_tab, s_text);
+            match_cnt = KMP_search(searchWordList[f], pre_tab, s_text);
             if(match_cnt != 0){
-                score+=(20*match_cnt);
+                score += (20 * match_cnt);
             }
 
             if(tipuesearch.pages[i].tags){
-                match_cnt = KMP_search(d_w[f], pre_tab, tipuesearch.pages[i].tags);
+                match_cnt = KMP_search(searchWordList[f], pre_tab, tipuesearch.pages[i].tags);
                 if(match_cnt != 0){
-                    score+=(10*match_cnt);
+                    score += (10 * match_cnt);
                 }
             }
-            match_cnt = KMP_search(d_w[f], pre_tab, tipuesearch.pages[i].url);
+            match_cnt = KMP_search(searchWordList[f], pre_tab, tipuesearch.pages[i].url);
             if(match_cnt != 0){
-                score+=20;
+                score += 20;
             }
-            if(score!=0){
-                for(var e=0;e<tipuesearch_weight.weight.length;e++){
-                    if(tipuesearch.pages[i].url==tipuesearch_weight.weight[e].url){
-                        score+=tipuesearch_weight.weight[e].score;
+            if(score != 0){
+                for(var e = 0; e < tipuesearch_weight.weight.length; e++){
+                    if(tipuesearch.pages[i].url == tipuesearch_weight.weight[e].url){
+                        score += tipuesearch_weight.weight[e].score;
                     }
                 }
             }
-            if(d_w[f].match("^-")){
-                pat=new RegExp(d_w[f].substring(1),"i");
-                if(KMP_search(d_w[f], pre_tab, tipuesearch.pages[i].title)!=0||KMP_search(d_w[f], pre_tab, s_text)!=0||KMP_search(d_w[f], pre_tab, tipuesearch.pages[i].tags)!=0){
+            if(searchWordList[f].match("^-")){
+                pat=new RegExp(searchWordList[f].substring(1),"i");
+                if(KMP_search(searchWordList[f], pre_tab, tipuesearch.pages[i].title) != 0 ||
+                   KMP_search(searchWordList[f], pre_tab, s_text) != 0 ||
+                   KMP_search(searchWordList[f], pre_tab, tipuesearch.pages[i].tags)!=0){
                     score=0;
                 }
             }
