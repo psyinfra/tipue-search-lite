6ead12655f6641b1672756486a22d2494bb2168d
Alex Waite <alex@waite.eu>
2021-10-14 07:32:15 +0200

    multiply page weight overrides (rather than add); simplify structure

--- tipuesearch_lite.js
+++ tipuesearch_lite.js
@@ -5,12 +5,12 @@
 const commonTerms = ["a", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"];
 
 // Weighting for tipue KMP algorithm
-const tipuesearchWeight = {'weight': [
-    {'url': 'http://www.tipue.com', 'score': 60},
-    {'url': 'http://www.tipue.com/search', 'score': 60},
-    {'url': 'http://www.tipue.com/tipr', 'score': 30},
-    {'url': 'http://www.tipue.com/support', 'score': 20}
-]};
+const tipuesearchWeights = {
+    'http://www.tipue.com': 3,
+    'http://www.tipue.com/search': 2,
+    'http://www.tipue.com/tipr': 1.5,
+    'http://www.tipue.com/support': 1.2
+};
 
 window.onload = function execute(){
     var set = {
@@ -256,12 +256,8 @@ window.onload = function execute(){
             if (match_cnt != 0) {
                 score += 20;
             }
-            if (score != 0) {
-                for (let e = 0; e < tipuesearchWeight.weight.length; e++) {
-                    if (page.url == tipuesearchWeight.weight[e].url) {
-                        score += tipuesearchWeight.weight[e].score;
-                    }
-                }
+            if (page.url in tipuesearchWeights) {
+                score = (score * tipuesearchWeights[page.url].score);
             }
             if (searchWord.match("^-")) {
                 pat=new RegExp(searchWord.substring(1),"i");
